1084d641bd8328f8531e7b53390e02f0765460bb
pratya.p <pedpratya@gmail.com>
2015-03-18 12:29:37 +0700

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,12 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+require __DIR__.'/routes/admin.php';
