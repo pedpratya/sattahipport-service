c825b8c42fdcd8e3b0c29ed5ede3a72adfe5d828
tom04 <thunayoshi04@gmail.com>
2015-03-19 16:53:03 +0700

    lihk'sohkadmin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,90 +1,20 @@
 @extends('layout.master')
 @section('breadcrumbs', Breadcrumbs::render('organization'))
 @section('content')
-<div class="container-fluid">
-            <div class="col-lg-12 col-md-12 ">
-                <div class="col-lg-10 col-md-10">
-                    <a href = "create-or-edit">
-                    <button type="button" class="btn btn-primary">เพิ่มหน่วยงาน</button>
-                    </a>
-                </div>
-                <div class="col-lg-2 col-md-2">
-                    <label>พิมพ์ข้อมูลทั้งหมด <button type="button" class="btn btn-default glyphicon glyphicon-print"></button></label>
-                </div>
+
+    <div class="container">
+        <div class="col-lg-10 col-md-10 page-header">
+            <div class="col-lg-10 col-md-10">
+                <label>คู่มือการใช้งานระบบ Admin</label>
             </div>
-        <div class="panel-body">
-        <form class="form-horizontal">
-            <div class="col-lg-6 col-md-6">
-                <div class="form-group">
-                    <div class="col-lg-1 col-md-1"><label>Code</label></div>
-                    <div class="col-lg-5 col-md-5"><input class="form-control input-sm" type="text" ></div>
-                </div>
+            <div class="col-lg-2 col-md-2">
+                <label>พิมพ์ข้อมูลทั้งหมด <button type="button" class="btn btn-default glyphicon glyphicon-print"></button></label>
             </div>
-            <div class="col-lg-6 col-md-6">
-                <div class="form-group">
-                    <div class="col-lg-1 col-md-1"><label>Active</label></div>
-                    <div class="col-lg-3 col-md-3"><input class="checkbox" type="checkbox" ></div>
-                </div>
-            </div>
+         </div>
+        <div class="col-lg-10 col-md-10">
             
-            <div class="col-lg-6 col-md-6">
-                <div class="form-group">
-                    
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6">
-                <div class="form-group">
-                    <div class="col-lg-1 col-md-1"><label>ชื่อ</label></div>
-                    <div class="col-lg-7 col-md-7"><input class="form-control input-sm" type="text" ></div>
-                    
-                </div>
-            </div>   
-        </form>
-  
+        </div>
+    </div>
 
-
-        
-        <div class="panel-body">
-        <div class="container-fluid">
-            <div class="col-lg-12 col-md-12">
-                <table class="table">
-                    <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>คำย่อ</th>
-                        <th>Code</th>
-                        <th>Description</th>
-                        <th>Edit</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>ท่าเรือสัตหีบ</td>
-                        <td>ทพส.-ทร</td>
-                        <td>T00</td>
-                        <td></td>
-                        <td><button type="button" class="btn btn-default glyphicon glyphicon-pencil"></button></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>การท่า</td>
-                        <td>-</td>
-                        <td>T100</td>
-                        <td></td>
-                        <td><button type="button" class="btn btn-default glyphicon glyphicon-pencil"></button></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Beer</td>
-                        <td>-</td>
-                        <td>T101</td>
-                        <td></td>
-                        <td><button type="button" class="btn btn-default glyphicon glyphicon-pencil"></button></td>
-                    </tr>
-                </table>  
-            </div>
-        </div>
-        </div>
-        </div>
-</div>
 
 @stop
