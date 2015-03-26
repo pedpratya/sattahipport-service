6c6820df9524ce358ddabb73501f307db917958b
arisaicbup53 <beer.cs53@hotmail.com>
2015-03-26 15:13:07 +0700

    up  route/customer

--- app/Http/routes/customer.php
+++ app/Http/routes/customer.php
@@ -8,10 +8,41 @@
 
 Route::group(array('prefix' => 'customer'), function() {
 
+    Route::get('index', array(
+    'as' => 'index',
+    function() {
+        return View::make('customer.index');
+    }));
+
     Route::get('customer-information', array(
         'as' => 'customer-information',
         function() {
             return View::make('customer.customer-information');
     }));
-    
+
+    Route::get('rental-space', array(
+        'as' => 'rental-space',
+        function() {
+            return View::make('customer.rental-space');
+    }));
+
+    Route::get('service-log', array(
+        'as' => 'service-log',
+        function() {
+            return View::make('customer.service-log');
+    }));
+
+    Route::get('vehicle-access', array(
+        'as' => 'vehicle-access',
+        function() {
+            return View::make('customer.vehicle-access');
+    }));
+
+    Route::get('ware-house', array(
+        'as' => 'ware-house',
+        function() {
+            return View::make('customer.ware-house');
+    }));
+
+
 });
