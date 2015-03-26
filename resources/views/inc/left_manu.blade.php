d6564f85fb7c0d7646c505207d63c04efef1d1f8
dringnakub@gmail.com <dringnakub@gmail.com>
2015-03-26 10:59:25 +0700

    แก้ไข เมนูย่อย

--- resources/views/inc/left_manu.blade.php
+++ resources/views/inc/left_manu.blade.php
@@ -1,58 +1,18 @@
 <!-- LEFT MENU -->
 <ul class="nav nav-sidebar">
-        <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#organization" class="collapsed active">องค์กร</li>
+    <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#organization" class="collapsed active"><a href="{{ URL::to('admin/organization')}}">องค์กร</a></li>
        <ul class="nav nav-sidebar nav-stacked collapse " id="organization">
-        <li>
-          <a href="{{ URL::to('admin/organization')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 1
-         </a>
-        </li>
-        <li>
-          <a href="{{ URL::to('admin/organization-create-or-edit')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 2
-          </a>
-        </li>
         </ul>
 
-        <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#system-admin" class="collapsed active">ระบบงาน</li>
+    <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#system-admin" class="collapsed active"><a href="{{ URL::to('admin/system-admin')}}">ระบบงาน</a></li>
             <ul class="nav nav-sidebar nav-stacked collapse " id="system-admin">
-        <li>
-          <a href="{{ URL::to('admin/system-admin')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 1
-         </a>
-        </li>
-        <li>
-          <a href="{{ URL::to('admin/system-admin-create-or-edit')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 2
-          </a>
-        </li>
             </ul>
 
-        <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#usermanagement" class="collapsed active">User Management</li>
+    <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#usermanagement" class="collapsed active"><a href="{{ URL::to('admin/usermanagement')}}">User Management</a></li>
             <ul class="nav nav-sidebar nav-stacked collapse " id="usermanagement">
-        <li class="active">
-          <a href="{{ URL::to('admin/usermanagement')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 1
-         </a>
-        </li>
-        <li>
-          <a href="{{ URL::to('admin/usermanagement-create-or-edit')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 2
-          </a>
-        </li>
-        <li>
-          <a href="{{ URL::to('admin/usermanagement-userlog')}}">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 3
-          </a>
-        </li>
             </ul>
-        <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#report" class="collapsed active">รายงาน</li>
+    <li class="nav-mainmenu hidden-tablet" data-toggle="collapse" data-target="#report" class="collapsed active"><a href="#">รายงาน</a></li>
             <ul class="nav nav-sidebar nav-stacked collapse " id="report">
-        <li>
-          <a href="#">
-                  <span class="glyphicon glyphicon-tasks"></span> เมนูย่อย 1
-         </a>
-        </li>
             </ul>
 </ul>
 <!--/.LEFT MENU -->
