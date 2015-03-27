afa96dddb170ad91e4b1c7be82cd3a002fddc9f9
arisaicbup53 <beer.cs53@hotmail.com>
2015-03-27 11:14:08 +0700

    edit admin/home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,54 +1,53 @@
 @extends('layout.master')
 @section('breadcrumbs', Breadcrumbs::render('admin-home'))
 @section('content')
-        <div class="col-lg-12 col-md-12 page-header">
-            <div class="col-lg-10 col-md-10">
-                <label>คู่มือการใช้งานระบบ Admin</label>
-            </div>
-            <div class="col-lg-2 col-md-2">
-                <label>พิมพ์ข้อมูลทั้งหมด <button type="button" class="btn btn-default glyphicon glyphicon-print"></button></label>
-            </div>
-         </div>
-        <div class="col-lg-12 col-md-12 bgAdmin">
-            <h4 class="text-center">ยินดีต้อนรับเข้าสู่ระบบผู้ดูแลระบบ Admin</h4>
-            <div class="form-group">
-                <div class="col-lg-12 col-md-12 row">
-                    <div class="col-lg-4 col-md-4">
-                        <button type="button" class="btn btn-default btn-block">องค์กรณ์</button>
-                    </div>
-                </div>  
-            
-                <div class="col-lg-12 col-md-12">
-                    <div class="col-lg-4 col-md-4">
-                        <button type="button" class="btn btn-default btn-block">เมนูระบบงาน</button>
-                    </div>
-                </div>    
-                
-                <div class="col-lg-12 col-md-12">
-                    <div class="col-lg-4 col-md-4">
-                        <button type="button" class="btn btn-default btn-block">เมนูกำหนดค่าคงที่</button>
-                    </div>
-                </div> 
-                
-                <div class="col-lg-12 col-md-12">
-                    <div class="col-lg-4 col-md-4">
-                        <button type="button" class="btn btn-default btn-block">Software Management</button>
-                    </div>
-                </div>
-                
-                <div class="col-lg-12 col-md-12">
-                    <div class="col-lg-4 col-md-4">
-                        <button type="button" class="btn btn-default btn-block">ผู้ใช้งานในระบบ</button>
-                    </div>
-                </div>
-                
-                <div class="col-lg-12 col-md-12">
-                    <div class="col-lg-4 col-md-4">
-                        <button type="button" class="btn btn-default btn-block">รายงานต่างๆ</button>
-                    </div>
-                </div>
-            </div> 
+
+<!-- header -->
+  <div class="col-lg-12 col-md-12 page-header">
+    <div class="col-lg-10 col-md-10">
+      <label>คู่มือการใช้งานระบบ Admin</label>
+    </div>
+    <div class="col-lg-2 col-md-2">
+      <label>พิมพ์ข้อมูลทั้งหมด <button type="button" class="btn btn-default glyphicon glyphicon-print"></button></label>
+    </div>
+  </div><!-- End header -->
+
+<!-- bedy -->
+  <div class="col-lg-12 col-md-12 bgAdmin">
+    <h4 class="text-center">ยินดีต้อนรับเข้าสู่ระบบผู้ดูแลระบบ Admin</h4>
+    <div class="form-group">
+      <div class="col-lg-12 col-md-12 row">
+        <div class="col-lg-4 col-md-4">
+          <button type="button" class="btn btn-default btn-block">องค์กรณ์</button>
         </div>
+      </div>  
+      <div class="col-lg-12 col-md-12">
+        <div class="col-lg-4 col-md-4">
+          <button type="button" class="btn btn-default btn-block">เมนูระบบงาน</button>
+        </div>
+      </div>    
+      <div class="col-lg-12 col-md-12">
+        <div class="col-lg-4 col-md-4">
+          <button type="button" class="btn btn-default btn-block">เมนูกำหนดค่าคงที่</button>
+        </div>
+      </div> 
+      <div class="col-lg-12 col-md-12">
+        <div class="col-lg-4 col-md-4">
+          <button type="button" class="btn btn-default btn-block">Software Management</button>
+        </div>
+      </div>
+      <div class="col-lg-12 col-md-12">
+        <div class="col-lg-4 col-md-4">
+          <button type="button" class="btn btn-default btn-block">ผู้ใช้งานในระบบ</button>
+        </div>
+      </div>
+      <div class="col-lg-12 col-md-12">
+        <div class="col-lg-4 col-md-4">
+          <button type="button" class="btn btn-default btn-block">รายงานต่างๆ</button>
+        </div>
+      </div>
+    </div> 
+  </div><!-- End body -->
 
 
 
