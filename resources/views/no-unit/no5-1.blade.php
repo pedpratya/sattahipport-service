db14c23478172fbed71264b468b7bcce011f4f60
tom04 <thunayoshi04@gmail.com>
2015-04-07 17:24:40 +0700

    แก้ไขรูปแบบฟอร์ม

--- resources/views/no-unit/no5-1.blade.php
+++ resources/views/no-unit/no5-1.blade.php
@@ -16,11 +16,13 @@
             <div class="form-group">
                 <div class="col-lg-6 col-md-6">                 
                     <div class="col-lg-3 col-md-3"><label>(DD/MM/YY) :&nbsp;</label></div>
-                    <div class="col-lg-8 col-md-8 input-group">
-                        <input type="text" class="form-control">
-                        <div class="input-group-btn">
-                          <button type="button" class="btn btn-default" data-toggle="modal" data-target="#"><span class="glyphicon glyphicon-calendar"></span></button> 
-                        </div>
+                    <div class="col-lg-8 col-md-8">
+                        <div class='input-group date'>
+                            <input type="text" class="form-control">
+                            <div class="input-group-btn">
+                              <button type="button" class="btn btn-default" data-toggle="modal" data-target="#"><span class="glyphicon glyphicon-calendar"></span></button> 
+                            </div>
+                        </div> 
                     </div>
                 </div>
             </div>
@@ -38,7 +40,7 @@
                         </div>
                         <label>&nbsp;&nbsp;ขาเข้า<br>&nbsp;&nbsp;Arrival</label>
                     </div>    
-                    <div class="col-lg-3 col-md-3">
+                    <div class="col-lg-4 col-md-4">
                         <div class="col-lg-1 col-md-1">
                             <input type="radio" name="" id="" value="" >
                         </div>
