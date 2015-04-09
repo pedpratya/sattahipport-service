5240c89214ec3663d087903c576e0ef03e1d15f8
tom04 <thunayoshi04@gmail.com>
2015-04-09 15:33:08 +0700

    สร้าง service

--- resources/views/service/home.blade.php
+++ resources/views/service/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.master-module')
-
+@section('breadcrumbs', Breadcrumbs::render('service-home'))
 @section('content')
 
 <!-- header -->
 <div class="row page-header">
   <div class="col-md-8 col-sm-8">
-    <label>คู่มือการใช้งานระบบ Admin</label>
+    <label>คู่มือการใช้งานระบ บบริการลูกเรือและผู้โดยสาร</label>
   </div>
   <div class="col-md-4 col-sm-4">
     <div class="col-md-4 col-sm-4"></div>
@@ -21,46 +21,29 @@
 
 <!-- bedy -->
   <div class="col-lg-12 col-md-12 bgAdmin">
-    <h4 class="text-center">ยินดีต้อนรับเข้าสู่ระบบผู้ดูแลระบบ Admin</h4>
+    <h4 class="text-center">ยินดีต้อนรับเข้าสู่ระบบ บริการลูกเรือและผู้โดยสาร</h4>
 
     <div class="form-group">
       <div class="col-lg-12 col-md-12">
-        <div class="col-lg-4 col-md-4">
-          <a href = "organization">
-            <button type="button" class="btn btn-primary btn-block">องค์กรณ์</button>
+        <div class="col-lg-7 col-md-7">
+          <a href = "">
+            <button type="button" class="btn btn-primary btn-block">กำหนดค่าเริ่มต้นให้กับเจ้าหน้าที่ปฏิบัติงาน และช่วงเวลาปฏิบัติงาน</button>
           </a>
         </div>
       </div>  
       <div class="col-lg-12 col-md-12">
-        <div class="col-lg-4 col-md-4">
-          <a href = "system-admin">
-            <button type="button" class="btn btn-primary btn-block">เมนูระบบงาน</button>
+        <div class="col-lg-7 col-md-7">
+          <a href = "">
+            <button type="button" class="btn btn-primary btn-block">รายการ เพื่อตรวจสอบลูกเรือและผู้โดยสาร</button>
           </a>
         </div>
       </div>    
       <div class="col-lg-12 col-md-12">
-        <div class="col-lg-4 col-md-4">
-          <button type="button" class="btn btn-primary btn-block">เมนูกำหนดค่าคงที่</button>
+        <div class="col-lg-7 col-md-7">
+          <button type="button" class="btn btn-primary btn-block">แสดงรายการลูกเรือและผู้โดยสาร ของแต่ละ Berth ที่ได้ดำเนินการเสร็จเรียบร้อย</button>
         </div>
       </div> 
-      <div class="col-lg-12 col-md-12">
-        <div class="col-lg-4 col-md-4">
-          <button type="button" class="btn btn-primary btn-block">Software Management</button>
-        </div>
-      </div>
-      <div class="col-lg-12 col-md-12">
-        <div class="col-lg-4 col-md-4">
-          <a href = "usermanagement">
-            <button type="button" class="btn btn-primary btn-block">ผู้ใช้งานในระบบ</button>
-          </a>
-        </div>
-      </div>
-      <div class="col-lg-12 col-md-12">
-        <div class="col-lg-4 col-md-4">
-          <button type="button" class="btn btn-primary btn-block">รายงานต่างๆ</button>
-        </div>
-      </div>
-    </div> 
+      
   </div><!-- End body -->
 
 @stop
