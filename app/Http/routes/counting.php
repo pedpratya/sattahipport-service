500b70ae50c7656435925d8ad21f4126f2579dd8
arisaicbup53 <beer.cs53@hotmail.com>
2015-04-09 15:14:19 +0700

    แก้ชื่อไฟล์ inspection เป็น counting

---
+++ app/Http/routes/counting.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Route::group(array('prefix' => 'counting'), function() {
+    
+    Route::get('home', array(
+        'as' => 'home',
+        function() {
+            return view('counting.home');
+    }));
+
+
+});
+
+	
