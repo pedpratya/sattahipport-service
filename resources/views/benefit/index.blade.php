4821b2d06b23ed5e806896b77a0742db852554f5
tom04 <thunayoshi04@gmail.com>
2015-04-10 12:38:31 +0700

    อัพเมนูผลประโชน์ และไอคอนเมนู

--- resources/views/benefit/index.blade.php
+++ resources/views/benefit/index.blade.php
@@ -4,37 +4,37 @@
 @section('content')
     <div class="container-fluid text-center">    
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-crm.png') }}">
-            <div class="box-menu-text-m">CRM</div>
+            <img src="{{ asset('img/icon-mainmenu/serviceRate-icon.png') }}">
+            <div class="box-menu-text-m">Port Tariff</div>
         </div>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-admin.png') }}">
-            <div class="box-menu-text-m">ADMIN</div>
+            <img src="{{ asset('img/icon-mainmenu/debtor-icon.png') }}">
+            <div class="box-menu-text-m">ทะเบียนคุมลูกหนี้</div>
         </div>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-portManage.png') }}">
-            <div class="box-menu-text-m">Port Manage</div>
+            <img src="{{ asset('img/icon-mainmenu/guarantee.png') }}">
+            <div class="box-menu-text-m">ทะเบียนคุมเงินหลักประกัน</div>
         </div>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-portManage.png') }}">
-            <div class="box-menu-text-m">Port Manage</div>
+            <img src="{{ asset('img/icon-mainmenu/pledge.png') }}">
+            <div class="box-menu-text-m">ทะเบียนคุมเงินมัดจำ</div>
         </div>
         <br>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-egate.png') }}">
-            <div class="box-menu-text-m">e - Gate</div>
+            <img src="{{ asset('img/icon-mainmenu/service-icon.png') }}">
+            <div class="box-menu-text-m">ทะเบียนคุมไฟฟ้า-ประปา</div>
         </div>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-security.png') }}">
-            <div class="box-menu-text-m">Security System</div>
+            <img src="{{ asset('img/icon-mainmenu/percenOff-icon.png') }}">
+            <div class="box-menu-text-m">คำนวนอัตราเบี้ยปรับ</div>
         </div>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-areaManame.png') }}">
-            <div class="box-menu-text-m">บริหารพื้นที</div>
+            <img src="{{ asset('img/icon-mainmenu/invoice-icon.png') }}">
+            <div class="box-menu-text-m">ออกใบแจ้งหนี้</div>
         </div>
         <div class="box-menu">
-            <img src="{{ asset('img/icon-mainmenu/icon-areaManame.png') }}">
-            <div class="box-menu-text-m">บริหารพื้นที</div>
+            <img src="{{ asset('img/icon-mainmenu/report-icon.png') }}">
+            <div class="box-menu-text-m">REPORT</div>
         </div>
     </div>
 @stop
